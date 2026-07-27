41463dcf0502f8cc7c8450b3d351433c5ab6412b
chandandigital6 <chandandigital6@gmail.com>
2026-07-28 00:32:41 +0530

    iamge

--- resources/views/front/groups_company.blade.php
+++ resources/views/front/groups_company.blade.php
@@ -960,7 +960,7 @@
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="grid items-center gap-10 lg:grid-cols-[.95fr_1.05fr] lg:gap-14">
                 <div class="relative overflow-hidden rounded-2xl border border-slate-200 bg-white p-3 shadow-lg">
-                    <img src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=1200&q=76"
+                    <img src="{{asset('assets/office img.png') }}"
                         alt="GPT Group shared business model" class="h-[300px] w-full rounded-xl object-cover sm:h-[360px]"
                         loading="lazy">
                 </div>
